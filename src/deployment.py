c31884f9b3d4836f14e0801dfceac20baf5cd5db
Admin <boda57622@gmail.com>
2025-08-24 15:30:57 +0300

    deploy

---
+++ src/deployment.py
@@ -0,0 +1,41 @@
+import os
+
+from psycopg.pq.misc import connection_summary
+
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = [os.environ('WEBSITE_HOSTNAME')]
+CSRD_TRASTED_ORIGINS = ['https://' + os.environ['WEBSITE_HOSTNAME']]
+DEBUG = False
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+STATIFILES_STORAGE = 'whitenoise.storage.CompressedManifestStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+connection_string = os.environ['AZURE_POSTGRES_CONNECTIONSTRING']
+parameters = {pair.split('='):pair.split('=')[1] for pair in connection_string.split(' ')}
+
+DATABASES = {
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresq1',
+        'NAME': parameters['dbname'],
+        'HOST': parameters['host'],
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
+
+    }
+
+}
